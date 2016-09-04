9ed7f8c431b817419bbfb71cdce1b6f71184ab01
Hillsrion <ismael.11@hotmail.fr>
2016-09-05 00:20:48 +0200

    Initial commit

---
+++ wp-content/themes/Jeanne/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<?php wp_head(); ?>
+<link rel="stylesheet" href="css/app.css">
+</head>
+<body <?php body_class(); ?>>
+    <header class="row header">
+        <div class="col-xs">
+            <nav class="row main__nav">
+                <ul class="main-nav__list">
+                    <li class="main-nav__item"></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
