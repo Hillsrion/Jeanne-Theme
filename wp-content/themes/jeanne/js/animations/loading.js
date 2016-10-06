3a1d78b628b0c3164d2b44965a31453c6c16e4e0
Hillsrion <ismael.11@hotmail.fr>
2016-10-06 08:45:22 +0200

    [Dev] WIP Entry animation

--- wp-content/themes/jeanne/js/animations/loading.js
+++ wp-content/themes/jeanne/js/animations/loading.js
@@ -9,11 +9,25 @@
     this.el = 'loading-page';
     this.$el = $('.'+this.el);
     this.$nameItems = $('.name__item',this.$el);
+    this.$loadingFill = $('.loading-bar__fill span',this.$el);
     this.tl = new TimelineLite();
+    this.$window = $(window);
+    this.$windowWidth = this.$window.width();
+    this.$windowHeight = this.$window.height();
   }
 
+    loadingSite.removeLoadingAnimation = function () {
+        this.$el.remove();
+    }
+
   loadingSite.fillTimeline = function () {
+    var ratioX = this.$windowWidth / this.$loadingFill.parent().width();
+    var ratioY = (this.$windowHeight / this.$loadingFill.parent().height())*1.2;
     this.tl.add(TweenLite.to(this.$nameItems,0.85,{x:0,delay:0.7,ease:Expo.easeOut}));
+    this.tl.add(TweenLite.to(this.$loadingFill,0.65,{scale:1, transformOrigin:"0px 0px",ease:Expo.easeInOut}));
+    this.tl.add(TweenLite.to(this.$nameItems,1.5,{x:70,delay:0.2, ease:Expo.easeOut}));
+    this.tl.add(TweenLite.to(this.$loadingFill,0.65,{scaleX: ratioX,transformOrigin:"50%",ease:Expo.easeInOut}));
+    this.tl.add(TweenLite.to(this.$loadingFill,0.5,{scaleY: ratioY,ease:Expo.easeInOut, onComplete: this.removeLoadingAnimation.bind(this)}));
   }
 
   loadingSite.init();
--- wp-content/themes/jeanne/partials/loading-animation.php
+++ wp-content/themes/jeanne/partials/loading-animation.php
@@ -1,22 +1,33 @@
 
-<div class="row loading-page">
-    <div class="name name--pre">
-        <span class="name__wrapper"><span class="name__item">J</span></span>
-        <span class="name__wrapper"><span class="name__item">e</span></span>
-        <span class="name__wrapper"><span class="name__item">a</span></span>
-        <span class="name__wrapper"><span class="name__item">n</span></span>
-        <span class="name__wrapper"><span class="name__item">n</span></span>
-        <span class="name__wrapper"><span class="name__item">e</span></span>
-    </div>
-    <div class="name name--last">
-        <span class="name__wrapper"><span class="name__item">P</span></span>
-        <span class="name__wrapper"><span class="name__item">l</span></span>
-        <span class="name__wrapper"><span class="name__item">o</span></span>
-        <span class="name__wrapper"><span class="name__item">u</span></span>
-        <span class="name__wrapper"><span class="name__item">n</span></span>
-        <span class="name__wrapper"><span class="name__item">e</span></span>
-        <span class="name__wrapper"><span class="name__item">v</span></span>
-        <span class="name__wrapper"><span class="name__item">e</span></span>
-        <span class="name__wrapper"><span class="name__item">z</span></span>
+<div class="loading-page">
+    <div class="row loading-page__wrapper">
+        <div class="loading-page__group">
+            <div class="name name--pre">
+                <span class="name__wrapper"><span class="name__item">J</span></span>
+                <span class="name__wrapper"><span class="name__item">e</span></span>
+                <span class="name__wrapper"><span class="name__item">a</span></span>
+                <span class="name__wrapper"><span class="name__item">n</span></span>
+                <span class="name__wrapper"><span class="name__item">n</span></span>
+                <span class="name__wrapper"><span class="name__item">e</span></span>
+            </div>
+            <div class="name name--last">
+                <span class="name__wrapper"><span class="name__item">P</span></span>
+                <span class="name__wrapper"><span class="name__item">l</span></span>
+                <span class="name__wrapper"><span class="name__item">o</span></span>
+                <span class="name__wrapper"><span class="name__item">u</span></span>
+                <span class="name__wrapper"><span class="name__item">n</span></span>
+                <span class="name__wrapper"><span class="name__item">e</span></span>
+                <span class="name__wrapper"><span class="name__item">v</span></span>
+                <span class="name__wrapper"><span class="name__item">e</span></span>
+                <span class="name__wrapper"><span class="name__item">z</span></span>
+            </div>
+        </div>
+        <div class="loading-page__group">
+            <div class="loading-bar">
+                <div class="loading-bar__fill">
+                    <span></span>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
